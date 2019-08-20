14c2eeef405fd32c1bad2dfab21cffa75bfd2aeb
NikitaKorzhov <KorzhovNikita@gmail.com>
2019-08-20 20:52:57 +0300

    Registration form

--- script.js
+++ script.js
@@ -10,4 +10,17 @@ $(()=>{
     messages:{nickname:{required:"Enter your Nikname"},
     password:{required:"Pasword vust be hear"}}
     });
+
+
+    $("#registrationForm").validate({ 
+        errorClass: "is-invalid",
+        submitHandler:(form,e)=>{
+            e.preventDefault();
+            alert($(form).serialize())
+            return false;
+          },
+ rules:{nickname:{required:true}},
+ messages:{nickname:{required:"Your nickname must be required"},password:{required:"Pasword vust be hear"}}
+ });
 })
+
